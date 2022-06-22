d9e97418c215cdb1d2e831190bafeb7150f80f2c
maheshpapolu <maheshnaidu9666@gmail.com>
2022-06-22 10:20:51 +0530

    [Mahesh]adding_Check_EmployeeWage_USING_SwitchCase

--- employee_dailywage_with_parttime.py
+++ employee_dailywage_with_parttime.py
@@ -1,6 +1,8 @@
 import random  # importing random modules
 
+
 print("<------------Calculating EmployeeDailyWage Based on Working Hours------------>\n")
+print("<-------Switch Case------->")
 
 
 class DailyWage:
@@ -9,28 +11,55 @@ class DailyWage:
         self.emp_daily_wage = emp_daily_wage
         self.emp_work_hour = emp_work_hour
 
-    # """
-    #    Defining a function name calculate Wage and declaring variables
+    """
+       Defining a function name calculate Wage and declaring variables
 
-    # """
+    """
 
     # Now checking the employee is present or not
+    def present_for_full_time(self):
+        """
+        this function is set to employee working hours as 8 hrs
+        :return: emp_work_hour
+        """
+        self.emp_work_hour = 8
+        return self.emp_work_hour
 
-    def get_working_hours(self):
-        check = random.randint(0, 2)
-        if check == 1:
-            result = random.randint(0, 1)
-            if result == 0:
-                print("Employee is present for part time")
-                self.emp_work_hour = 4
-            return self
+    def present_for_part_time(self):
+        """
+        this function is set to employee working hours as 4 hrs
+        :return: emp_work_hour
+        """
+        self.emp_work_hour = 4
+        return self.emp_work_hour
+
+    def employee_absent(self):
+        """
+        this function is set to employee as absent
+        :return: emp_work_hour
+        """
+        self.emp_work_hour = 0
+        return self.emp_work_hour
+
+    def switch_case(self):
+        """
+        implementing switch case in this function
+        :return: emp_status
+        """
+        switch = {
+            1: self.present_for_full_time(),
+            0: self.present_for_part_time()
+        }
+        return switch.get(self, "")()
+
+    def get_status(self):
+        self.emp_work_hour
+        check = random.randint(0, 1)
+        if not check:
+            return self.employee_absent()
         else:
-            print("Employee is present for full time")
-            self.emp_work_hour = 8
-            return self
-        print("Employee is absent")
-        emp_work_hour = 0
-        return self
+            check = random.randint(0, 1)
+            result = self.switch_case(0)
 
     def employee_daily_wage(self):
         print(self.emp_work_hour * self.wage_per_hour)
@@ -39,5 +68,5 @@ class DailyWage:
 
 if __name__ == '__main__':
     calculate_wage = DailyWage(20, 0, 0)
-    calculate_wage.get_working_hours().employee_daily_wage()
+    calculate_wage.employee_daily_wage()
     # Calculate the employee daily wage using random result
